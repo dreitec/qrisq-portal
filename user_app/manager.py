b385c870dcb7503b36373609c720af64e6097500
SumedhOutcode <sshakya@outcodesoftware.com>
2021-02-10 13:44:10 +0545

    master: Fixed _create_user method params

--- user_app/manager.py
+++ user_app/manager.py
@@ -4,19 +4,19 @@ from django.contrib.auth.base_user import BaseUserManager
 class UserManager(BaseUserManager):
     user_in_migrations = True
  
-    def _create_user(self, first_name, last_name, email, password, **extra_fields):
+    def _create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError('The given email must be set')
  
         email = self.normalize_email(email)
-        user = self.model(first_name=first_name, last_name=last_name, email=email, **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
  
-    def create_user(self, first_name, last_name, email, confirm_password, password=None, **extra_fields):
+    def create_user(self, email, confirm_password, password=None, **extra_fields):
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(first_name, last_name, email, password, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
  
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_superuser', True)
