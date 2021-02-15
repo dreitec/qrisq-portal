e05264b55f518614afebd8d121d2ac0bef79b209
SumedhOutcode <sshakya@outcodesoftware.com>
2021-02-15 15:06:24 +0545

    master: Fixed user signup issue

--- user_app/serializers/signup.py
+++ user_app/serializers/signup.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 from user_app.models import User, UserProfile, NUMERIC_VALIDATOR
 from user_app.utils import mail_sender
@@ -20,7 +21,7 @@ class SignupSerializer(serializers.Serializer):
     def validate(self, data):
         error = {}
         if not data['password'] == data['confirm_password']:
-            error['password_confirmation'] = "Passwords didn't match."
+            error['confirm_password'] = "Passwords didn't match."
         
         if User.objects.filter(email=data['email']).exists():
             error['email'] = "User with this email exists."
@@ -38,8 +39,9 @@ class SignupSerializer(serializers.Serializer):
         password = validated_data.pop('password')
 
         user = User.objects.create_user(email=email,
-                                        password=password,
                                         **{"first_name": first_name, "last_name": last_name})
+        user.set_password(password)
+        user.save()
 
         UserProfile.objects.create(user=user, **validated_data)
 
