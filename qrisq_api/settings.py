a2c85f96baf5f0bea5917d1b5271393e3e0cf0d5
Avik Pradhan <Apradhan@outcodesoftware.com>
2021-02-18 16:06:09 +0545

    Fix: Fix settings file.

--- qrisq_api/settings.py
+++ qrisq_api/settings.py
@@ -2,10 +2,7 @@ from pathlib import Path
 from datetime import timedelta
 
 import os
-if os.name == 'nt':
-    VENV_BASE = os.environ['VIRTUAL_ENV']
-    os.environ['PATH'] = os.path.join(VENV_BASE, 'Lib\\site-packages\\osgeo') + ';' + os.environ['PATH']
-    os.environ['PROJ_LIB'] = os.path.join(VENV_BASE, 'Lib\\site-packages\\osgeo\\data\\proj') + ';' + os.environ['PATH']
+
 from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
