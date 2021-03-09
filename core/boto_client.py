428d1cad8fa0a14a54dbdba0552f1202922fb4d5
SumedhOutcode <sshakya@outcodesoftware.com>
2021-03-09 14:22:00 +0545

    service_area: Updated boto_client

--- core/boto_client.py
+++ core/boto_client.py
@@ -1,37 +1,50 @@
+import os
 import logging
 
 from django.conf import settings
 
 import boto3
-from botocore.exceptions import ClientError, NoCredentialsError
+from botocore.exceptions import ClientError
 
 # logger = logging.getLogger()
 # logger.setLevel(logging.INFO)
 
 
 def __s3_client():
-    try:
-        client = boto3.client(
-            's3',
-            aws_access_key_id=settings.AWS_ACCESS_KEY,
-            aws_secret_access_key=settings.AWS_SECRET_KEY,
-            region_name=settings.AWS_REGION
-        )
-    except (ClientError, NoCredentialsError) as error:
-        print(error.response['Error']['Message'])
-    else:
-        return client
+    return boto3.client(
+        's3',
+        aws_access_key_id=settings.AWS_ACCESS_KEY,
+        aws_secret_access_key=settings.AWS_SECRET_KEY,
+        region_name=settings.AWS_REGION
+    )
 
 
 def download_file(filename):
     try:
         s3 = __s3_client()
-        bucket = 'qrisq-webapp-data-v2'
+        bucket = settings.AWS_WKT_BUCKET
         with open(filename, 'wb') as f:
             s3.download_fileobj(bucket, filename, f)
     except ClientError as err:
-        print("Client Error while downloading files")
+        os.remove(filename)
+        print(filename + ' WKT file not downloaded; ClientError: ' + err.response['Error']['Message'])
+        raise err
         # logger.info(filename + ' WKT file not downloaded; ClientError: ' + err.response['Error']['Message'])
     else:
         # logger.info(filename + " WKT file downloaded")
         print(filename + " WKT file downloaded")
+
+
+def upload_file(filename):
+    try:
+        s3 = __s3_client()
+        bucket = settings.AWS_WKT_BUCKET
+        s3.upload_file(filename, bucket)
+    except ClientError as err:
+        print(filename + ' WKT file not uploaded; ClientError: ' + err.response['Error']['Message'])
+        raise err
+        # logger.info(filename + ' WKT file not downloaded; ClientError: ' + err.response['Error']['Message'])
+    else:
+        # logger.info(filename + " WKT file downloaded")
+        print(filename + " WKT file uploaded")
+    pass
