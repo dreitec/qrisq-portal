6b1fea6e7c1c939aaea55b8a015edf10a9a4767f
SumedhOutcode <sshakya@outcodesoftware.com>
2021-04-28 15:55:01 +0545

    add-address-fluidpay: Refactored Paypal refund method

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from subscriptions.views import SubscriptionPlanViewSet, RefundPaymentView, CancelSubscriptionView, \
-    AddPaymentInfoView, FluidPayTransaction, fluidpay_refund
+    AddPaymentInfoView, FluidPayTransaction  # , fluidpay_refund
 
 router = SimpleRouter(trailing_slash=False)
 router.register('subscription-plans', SubscriptionPlanViewSet, basename="subscription-plans")
@@ -12,14 +12,8 @@ router.register('subscription-plans', SubscriptionPlanViewSet, basename="subscri
 urlpatterns = router.urls
 
 urlpatterns += [
-    # path("paypal/order", create_order, name="paypal-create-order"),
-    # path("paypal/approve/<str:order_id>", approve_order, name="paypal-approve-order"),
-    # path("paypal/capture/<str:order_id>", capture_order, name="paypal-capture-order"),
-    path("refund-payment", RefundPaymentView.as_view(), name="refund-order"),
+    path("refund-payment", RefundPaymentView.as_view(), name="refund-payment"),
     path("cancel-subscription", CancelSubscriptionView.as_view(), name="cancel-subscription"),
-
-    path("fluidpay/process-transaction", FluidPayTransaction.as_view(), name="fluid_pay_process_transaction"),
-    # path("fluidpay/refund-transaction", fluidpay_refund, name="fluid_pay_refund_transaction"),
-
+    path("process-transaction", FluidPayTransaction.as_view(), name="credit-card-process"),
     path('add-payment-info', AddPaymentInfoView.as_view(), name="add_payment_info"),
 ]
