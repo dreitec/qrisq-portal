da13ea77e4a8854b6f3070f9202f63192cf67ab9
SumedhOutcode <sshakya@outcodesoftware.com>
2021-04-30 14:21:44 +0545

    storm-data: Zip file creator for surge files

--- storm/storm_file_handler.py
+++ storm/storm_file_handler.py
@@ -1,18 +1,20 @@
-import math
 import gzip
 import json
 import logging
+import os
+from os.path import basename
 import re
+from zipfile import ZipFile
 
-from core.boto_client import download_storm_file, list_storm_folders
+# from core.boto_client import list_storm_folders
 
 logger = logging.getLogger(__name__)
 
 
-def download_latest_files():
-    result = list_storm_folders()
-    for o in result.get('CommonPrefixes'):
-        print(o.get('Prefix'))
+# def download_latest_files():
+#     result = list_storm_files(prefix="most-recent/")
+#     for o in result.get('CommonPrefixes'):
+#         print(o.get('Prefix'))
 
 
 def compressed_geojson_parser(geojson_compressed_filename):
@@ -33,3 +35,18 @@ def wind_js_parser(filename):
             return json.dumps(json.loads(json_string))
     except FileNotFoundError as err:
        pass
+
+
+def surge_zip_creator():
+    files = os.listdir('storm_files')
+    surge_files = [fil for fil in files if fil.startswith('surge')]
+
+    if not os.path.isdir('zip'):
+        os.makedirs('zip')
+    zipname = f"zip/{surge_files[0].split('.')[0]}.zip"
+    
+    with ZipFile(zipname, 'w') as zipobj:
+        for filename in surge_files:
+            filepath = os.path.join('storm_files', filename)
+            zipobj.write(filepath, basename(filepath))
+    return zipname
