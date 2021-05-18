6b2902cd8ed9bafb4ec418f7bae84d0f4e3b45fc
SumedhOutcode <sshakya@outcodesoftware.com>
2021-05-18 23:19:11 +0545

    develop: Updated Numeric validator regex

--- core/validators.py
+++ core/validators.py
@@ -7,4 +7,4 @@ CVC_VALIDATOR = RegexValidator(r'(\d{3,4})', 'Only numeric characters')
 
 DATE_VALIDATOR = RegexValidator(r'^((0?[1-9]|[1][0-2])\/\d{2})', 'Invalid Expiration date format')
 
-NUMERIC_VALIDATOR = RegexValidator(r'^[0-9+]', 'Only numeric characters')
+NUMERIC_VALIDATOR = RegexValidator(r'^[0-9]*$', 'Only numeric characters')
