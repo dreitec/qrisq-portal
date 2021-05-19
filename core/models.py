2592be5463eeb42417ef23ffe8962ced2f6c0e99
SumedhOutcode <sshakya@outcodesoftware.com>
2021-05-19 16:12:08 +0545

    develop: updated production.sh file

--- core/models.py
+++ core/models.py
@@ -6,4 +6,3 @@ class WktFile(models.Model):
     filename = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
--- run_production.sh
+++ run_production.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-python manage.py wait_for_db && python manage.py migrate && gunicorn qrisq_api.wsgi:application --bind 0.0.0.0:8000
+python manage.py wait_for_db && \
+    python manage.py migrate --no-input && \
+    python manage.py collectstatic --no-input && \
+    gunicorn --workers=4 qrisq_api.wsgi:application --bind 0.0.0.0:8000
