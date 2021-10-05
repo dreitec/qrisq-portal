4b341ca3bfe06f2e0eac5d28e0224c59e3b53649
Ellen Carriere <39715787+ellencarriere@users.noreply.github.com>
2021-10-05 09:56:59 -0500

    WIP - copilot setup (#12)
    * add manifest and workspace
    
    * update storage
    
    * progress
    
    * dockerfile
    
    * Use environment variables in prod
    
    * increase the budgets for prod builds
    
    * Frontend manifest
    
    * New app name and staging env
    
    * updating with new environment variables
    
    * hardcoded urls
    
    * hardcode urls
    
    * pipeline files
    
    * clean up code
    
    * Update pipeline.yml
    
    * Update manifest.yml
    
    Co-authored-by: Luke Ledet <luke@revelry.co>

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: true,
   staging: false,
-  API_URL: 'https://api.qrisq.com/api',
-};
+  API_URL: 'https://portal-api.staging.apps.qrisq.com/api',
+  HURRICANE_WIND_URL: 'https://qrisq-angular-webspa.s3.us-east-2.amazonaws.com/wind-2020-al28-17-202010282100.json',
+  HURRICANE_WIND_LINE_URL: 'https://qrisq-angular-webspa.s3.us-east-2.amazonaws.com/line-2020-al28-17-202010282100.json',
+  HURRICANE_WIND_POINTS_URL: 'https://qrisq-angular-webspa.s3.us-east-2.amazonaws.com/points-2020-al28-17-202010282100.json',
+  HURRICANE_WIND_POLYGON_URL: 'https://qrisq-angular-webspa.s3.us-east-2.amazonaws.com/polygon-2020-al28-17-202010282100.json'
+}
