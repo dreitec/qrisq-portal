3f7373bd1996450ceb106ba9f3d1d1c06219a9ec
Akintunde Salako Pounds <akinpds@gmail.com>
2021-11-29 15:07:21 -0600

    changed the api url from localhost to staging

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   staging: false,
-  API_URL: 'http://localhost:8000/api',
+  API_URL: 'https://portal-api.staging.apps.qrisq.com/api',
   HURRICANE_WIND_URL: 'https://qrisq-webapp-data-dev-v2-copy.s3.amazonaws.com/2021-al14-08/wind-2021-al14-08-202109140300.json',
   HURRICANE_WIND_LINE_URL: 'https://qrisq-webapp-data-dev-v2-copy.s3.amazonaws.com/2021-al14-08/line-2021-al14-08-202109140300.json',
   HURRICANE_WIND_POINTS_URL: 'https://qrisq-webapp-data-dev-v2-copy.s3.amazonaws.com/2021-al14-08/points-2021-al14-08-202109140300.json',
