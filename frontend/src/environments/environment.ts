099f3c077f2f47905ed900143a9149f8bc913407
Akintunde Salako Pounds <akinpds@gmail.com>
2021-11-29 11:25:31 -0600

    changed wind line, points, and polygon for env

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   staging: false,
-  API_URL: 'https://portal-api.staging.apps.qrisq.com/api',
-  HURRICANE_WIND_URL: 'https://qrisq-webapp-data-dev-v2-copy.s3.amazonaws.com/2021-al08-19/wind-2021-al08-19-202108201500.json',
-  HURRICANE_WIND_LINE_URL: 'https://qrisq-webapp-data-dev-v2-copy.s3.amazonaws.com/2021-al08-19/line-2021-al08-19-202108201500.json',
-  HURRICANE_WIND_POINTS_URL: 'https://qrisq-webapp-data-dev-v2-copy.s3.amazonaws.com/2021-al08-19/points-2021-al08-19-202108201500.json',
-  HURRICANE_WIND_POLYGON_URL: 'https://qrisq-webapp-data-dev-v2-copy.s3.amazonaws.com/2021-al08-19/polygon-2021-al08-19-202108201500.json',
+  API_URL: 'http://localhost:4200',
+  HURRICANE_WIND_URL: 'https://qrisq-webapp-data-dev-v2-copy.s3.amazonaws.com/2021-al14-08/wind-2021-al14-08-202109140300.json',
+  HURRICANE_WIND_LINE_URL: 'https://qrisq-webapp-data-dev-v2-copy.s3.amazonaws.com/2021-al14-08/line-2021-al14-08-202109140300.json',
+  HURRICANE_WIND_POINTS_URL: 'https://qrisq-webapp-data-dev-v2-copy.s3.amazonaws.com/2021-al14-08/points-2021-al14-08-202109140300.json',
+  HURRICANE_WIND_POLYGON_URL: 'https://qrisq-webapp-data-dev-v2-copy.s3.amazonaws.com/2021-al14-08/polygon-2021-al14-08-202109140300.json',
 }
