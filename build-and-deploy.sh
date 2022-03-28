4125c0a987aa04e8fbec7e2e533bb1eb9bb98d08
thomas-kantowski <tkantowski@gmail.com>
2022-03-28 15:51:55 -0600

    Test aws iam authenticator via codebuild.

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -11,21 +11,21 @@ UI_IMAGE=${AWS_ACCOUNT_NUMBER}.dkr.ecr.${AWS_REGION}.amazonaws.com/qrisq-ui
 API_IMAGE=${AWS_ACCOUNT_NUMBER}.dkr.ecr.${AWS_REGION}.amazonaws.com/qrisq-api
 MOST_RECENT_UI=$(aws ecr describe-images --output json --repository-name qrisq-ui --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' | jq . --raw-output)
 MOST_RECENT_API=$(aws ecr describe-images --output json --repository-name qrisq-api --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' | jq . --raw-output)
+#
+#docker pull ${UI_IMAGE}:${MOST_RECENT_UI}
+#docker pull ${API_IMAGE}:${MOST_RECENT_API}
 
-docker pull ${UI_IMAGE}:${MOST_RECENT_UI}
-docker pull ${API_IMAGE}:${MOST_RECENT_API}
-
-DOCKER_TAG_UI=${UI_IMAGE}:${VERSION}
-DOCKER_TAG_API=${API_IMAGE}:${VERSION}
+DOCKER_TAG_UI=${UI_IMAGE}:dev200
+DOCKER_TAG_API=${API_IMAGE}:dev200
 
 PROCESSING_TEMPLATE=$(cat deployment.yaml | sed "s|{{DOCKER_IMAGE_UI}}|${DOCKER_TAG_UI}|g" | sed "s|{{DOCKER_IMAGE_API}}|${DOCKER_TAG_API}|g")
 
-docker build . -t ${DOCKER_TAG_API} --cache-from ${API_IMAGE}:${MOST_RECENT_API}
-docker push ${DOCKER_TAG_API}
-
-cd frontend
-
-docker build . -t ${DOCKER_TAG_UI} --cache-from ${UI_IMAGE}:${MOST_RECENT_UI}
-docker push ${DOCKER_TAG_UI}
+#docker build . -t ${DOCKER_TAG_API} --cache-from ${API_IMAGE}:${MOST_RECENT_API}
+#docker push ${DOCKER_TAG_API}
+#
+#cd frontend
+#
+#docker build . -t ${DOCKER_TAG_UI} --cache-from ${UI_IMAGE}:${MOST_RECENT_UI}
+#docker push ${DOCKER_TAG_UI}
 
 echo "$PROCESSING_TEMPLATE" | kubectl apply -f -
