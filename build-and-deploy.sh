113cdd4414a6fc302cfcfcf1f3d9b671603eea8e
thomas-kantowski <tkantowski@gmail.com>
2022-03-28 15:02:22 -0600

    Pull most recent image for caching.

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -3,6 +3,7 @@ set -e
 
 AWS_ACCOUNT_NUMBER=$(aws sts get-caller-identity --query Account --output text)
 AWS_REGION="us-east-1"
+aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_NUMBER}.dkr.ecr.${AWS_REGION}.amazonaws.com
 
 VERSION=$1
 
@@ -18,8 +19,6 @@ DOCKER_TAG_UI=${UI_IMAGE}:${VERSION}
 DOCKER_TAG_API=${API_IMAGE}:${VERSION}
 
 PROCESSING_TEMPLATE=$(cat deployment.yaml | sed "s|{{DOCKER_IMAGE_UI}}|${DOCKER_TAG_UI}|g" | sed "s|{{DOCKER_IMAGE_API}}|${DOCKER_TAG_API}|g")
-
-aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_NUMBER}.dkr.ecr.${AWS_REGION}.amazonaws.com
 
 docker build . -t ${DOCKER_TAG_API}
 docker push ${DOCKER_TAG_API}
