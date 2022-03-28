279680315957f58383b46a2fb0df2284815d3b95
thomas-kantowski <tkantowski@gmail.com>
2022-03-28 14:59:01 -0600

    Pull most recent image for caching.

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -6,8 +6,16 @@ AWS_REGION="us-east-1"
 
 VERSION=$1
 
-DOCKER_TAG_UI=${AWS_ACCOUNT_NUMBER}.dkr.ecr.${AWS_REGION}.amazonaws.com/qrisq-ui:${VERSION}
-DOCKER_TAG_API=${AWS_ACCOUNT_NUMBER}.dkr.ecr.${AWS_REGION}.amazonaws.com/qrisq-api:${VERSION}
+UI_IMAGE=${AWS_ACCOUNT_NUMBER}.dkr.ecr.${AWS_REGION}.amazonaws.com/qrisq-ui
+API_IMAGE=${AWS_ACCOUNT_NUMBER}.dkr.ecr.${AWS_REGION}.amazonaws.com/qrisq-api
+MOST_RECENT_UI=$(aws ecr describe-images --output json --repository-name qrisq-ui --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' | jq . --raw-output)
+MOST_RECENT_API=$(aws ecr describe-images --output json --repository-name qrisq-api --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' | jq . --raw-output)
+
+docker pull ${UI_IMAGE}:${MOST_RECENT_UI}
+docker pull ${API_IMAGE}:${MOST_RECENT_API}
+
+DOCKER_TAG_UI=${UI_IMAGE}:${VERSION}
+DOCKER_TAG_API=${API_IMAGE}:${VERSION}
 
 PROCESSING_TEMPLATE=$(cat deployment.yaml | sed "s|{{DOCKER_IMAGE_UI}}|${DOCKER_TAG_UI}|g" | sed "s|{{DOCKER_IMAGE_API}}|${DOCKER_TAG_API}|g")
 
