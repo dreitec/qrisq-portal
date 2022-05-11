66ef120a58cffc95d60c9da30e55dfdb4a49640c
dreitec <herzogdev@gmail.com>
2022-05-11 14:43:47 -0400

    feat: deploy again

--- run_production.sh
+++ run_production.sh
@@ -2,5 +2,6 @@
 
 python manage.py wait_for_db && \
     python manage.py migrate --no-input && \
+    python manage.py migrate storm --database=storm --no-input && \
     python manage.py collectstatic --no-input && \
     gunicorn --workers=4 qrisq_api.wsgi:application --bind 0.0.0.0:8000 --timeout 120
