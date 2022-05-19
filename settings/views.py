4081a1d7ff403a12b038411eb1d12bfb413d0720
dreitec <herzogdev@gmail.com>
2022-05-19 11:58:31 -0400

    fix: add default config if no config data

--- settings/views.py
+++ settings/views.py
@@ -7,14 +7,29 @@ from user_app.permissions import IsAdminUser
 
 class GlobalConfigViewSet(APIView):
     permission_classes = (IsAuthenticated,)
-
+    response_data = {}
     def get(self, request, *args, **kwargs):
-        global_config = GlobalConfig.objects.all()
-        serializer = GlobalConfigSerializer(global_config[0])
-        return Response(serializer.data)
+        global_config = GlobalConfig.objects.all().order_by('-id')
+        if len(global_config) < 1:
+            default_config = {
+                "lookback_period": 24,
+                "lookback_override": False,
+                "active_storm": False,
+                "geocode_users": False,
+            }
+            serializer = GlobalConfigSerializer(data=default_config)
+            serializer.is_valid(raise_exception=True)
+            try:
+                serializer.save()
+            except Exception as error:
+                response_data = {}
+        else:
+            serializer = GlobalConfigSerializer(global_config[0])
+            response_data = serializer.data
+        return Response(response_data)
 
     def post(self, request, *args, **kwargs):
-        global_config = GlobalConfig.objects.get(pk=1)
+        global_config = GlobalConfig.objects.all().order_by('-id')[0]
         global_config.lookback_period = request.data.get('lookback_period')
         global_config.lookback_override = request.data.get('lookback_override')
         global_config.active_storm = request.data.get('active_storm')
